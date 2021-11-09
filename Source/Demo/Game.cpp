79ddf541f8b6ce36bcbd66b99f77a5162dec5cea
Ewan <StrikerKagami@Gmail.com>
2021-11-09 01:47:21 +0000

    Checklist
    A number of simple tasks to improve the codebase, now that I've met a milestone. 
    This should ALL be done before fully moving onto 3D Rendering. It's on a branch for a reason, though; Aiming to complete this all by the time I move onto loading 3D models.

--- Source/Demo/Game.cpp
+++ Source/Demo/Game.cpp
@@ -19,13 +19,25 @@ void Game::Init()
     m_Components.push_back(fpsComponent);
 
     keyboardView = new Engine::TextComponent(m_Graphics.get());
-    keyboardView->SetPosition(500, 500);
-    keyboardView->SetText(m_Keyboard->GetKeyboardState().to_string());
+    keyboardView->SetPosition(0, 660);
+    keyboardView->SetColor(0.05, 0.2, 1.0, 1.0);
+    m_Components.push_back(keyboardView);
+
+    Engine::TextComponent* warning = new  Engine::TextComponent(m_Graphics.get());
+    warning->SetText("DON'T EVEN THINK ABOUT MOVING ONTO\nRENDERING BEFORE YOU FULLY IMPLEMENT AND TEST\nALL THIS BACKEND STUFF!!!\n- Keyboard Input\n- Mouse Input\n- Building Fonts\n- Object Pooling\n- Camera Matrices\n- Framerate Limiting\n- Game Objects\n- Byte Alignment\n- OPTIMISATIONS\n- Warnings\n- Memory Leaks\n- COM Pointers\n- Sprite Component\n- COMMENTING\n- ARCHITECTURAL DESIGN DOCUMENT!!!!!!!!!\n- ...and tidying up :D\nHave Fun <3");
+    warning->SetColor(1, 0, 0.05, 1.0f);
+    warning->SetPosition(800, 10);
+    m_Components.push_back(warning);
 }
 
 //Update is called once per frame. 
 void Game::Update(float dt)
 {
+    //TODO: Format this properly (16 rows, 2 columns, 8 bits per column)
+    std::stringstream out;
+    std::string kbd = m_Keyboard->GetKeyboardState().to_string();
+    out << "Keyboard State: " << kbd.substr(0, 32) << std::endl << kbd.substr(32, 64) << std::endl << kbd.substr(64, 96) << std::endl << kbd.substr(96, 128) << std::endl << kbd.substr(128, 160) << std::endl;
+    keyboardView->SetText(out.str());
 }
 
 //FixedUpdate is called at a constant rate.
